c3166b041002dcf5f29f567ba547a214364074b6
zahra kh <zahra.kh.dev@gmail.com>
2024-01-10 18:00:14 +0330

    create dart project

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const Home() ,
+    );
+  }
+}
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          child: Image.asset("assets/images/digi.jpg"),
+        ),
+      ),
+    );
+  }
+}
